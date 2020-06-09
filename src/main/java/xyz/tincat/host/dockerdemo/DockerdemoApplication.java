8008925c11a26d0e7a605c76852b74d5258fbe48
shb <shb@tincat.xyz>
2020-06-09 09:25:51 +0800

    add date to msg

--- src/main/java/xyz/tincat/host/dockerdemo/DockerdemoApplication.java
+++ src/main/java/xyz/tincat/host/dockerdemo/DockerdemoApplication.java
@@ -6,16 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SpringBootApplication
 @RestController
 public class DockerdemoApplication {
-
+    private static String msg;
     @RequestMapping(path = "hello",method = RequestMethod.GET)
     public String test() {
-        return "hello world!";
+        return "Hello world! This app start at： "+msg;
     }
 
 	public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        sdf.applyPattern("yyyy-MM-dd HH:mm:ss");
+                msg = sdf.format(new Date());
 		SpringApplication.run(DockerdemoApplication.class, args);
 	}
 
